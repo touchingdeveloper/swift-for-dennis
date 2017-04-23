f069287d48e4bfdaa961fd2b8155ae7c822bb12a
touchingdeveloper <touchingdeveloper@gmail.com>
2017-04-23 15:14:49 +0900

    Add collection types

---
+++ 03_collection_types.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+// array (list)
+var someInts = [Int]()
+someInts.append(1)
+someInts.append(2)
+var someStrings: [String] = ["hello", "hi", "nihao"]
+// var someStrings: [String] = ["hello", "hi", "nihao"]
+someStrings.append("an")
+someStrings[0]
+someStrings[3]
+someStrings[1] = "hii"
+someStrings
+someStrings[0...2]
+
+
+// tuple
+var tupleInts = (1, 2, 3)
+// tupleInts.append(4)
+
+
+// set
+// dictionary (hash map)
+
